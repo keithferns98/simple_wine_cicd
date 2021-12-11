27071cbe77c2c2b067fd23c9b6e1f57659002c25
Keith <keithfernandes311#gmail.com>
2021-12-11 20:24:24 +0530

    add get_data

--- src/get_data.py
+++ src/get_data.py
@@ -14,12 +14,15 @@ def read_params(config_path):
 
 def get_data(config_path):
     config=read_params(config_path)
-    print(config)
-    # data_path= 
+    # print(config)
+    data_path= config['data_source']['s3_source']
+    df=pd.read_csv(data_path,sep=",",encoding='utf-8')
+    return df 
     
 
 if __name__=="__main__":
     args=argparse.ArgumentParser()
     args.add_argument("--config",default="params.yaml")
     parsed_args=args.parse_args()
+    print(parsed_args.config)
     data = get_data(parsed_args.config)
